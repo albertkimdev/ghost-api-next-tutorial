6fd25bfb739e37ebcc6cd05b3679d1bb7ce63995
yongelee <albertkim91@gmail.com>
2020-07-30 13:46:31 +0800

    step 2 done

--- ghost/ghostData.js
+++ ghost/ghostData.js
@@ -1,5 +1,35 @@
-const getBlogData = () => {
-  console.log("Ghost data");
+const GhostContentAPI = require("@tryghost/content-api");
+
+const api = new GhostContentAPI({
+  url: "http://localhost:2368",
+  key: "93a23363ce36ec868dfde5660b",
+  version: "v3",
+});
+
+async function getPosts() {
+  return await api.posts
+    .browse({
+      limit: "all",
+      include: "tags,authors",
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+async function getSinglePost(postSlug) {
+  return await api.posts
+    .read({
+      slug: postSlug,
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+
+const runit = async () => {
+  const post = await getSinglePost("welcome");
+  console.log(post);
 };
 
-getBlogData();
+runit();
